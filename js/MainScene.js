2674fe9e705149b0c8fa302fbc72cb7cf5afc4c0
Jeff Jacob <phansoft@gmail.com>
2025-01-26 11:05:29 -0800

    Switching from rectable to sprite for multiplier

--- js/MainScene.js
+++ js/MainScene.js
@@ -1,6 +1,7 @@
 import {PlayField} from "./mech/PlayField.js"
 import {Spawner} from './mech/Spawner.js';
 import {Combiner} from './mech/Combiner.js'
+import {Multiplier} from './mech/Multiplier.js'
 import {HUD} from './mech/HUD.js'
 
 export class MainScene extends Phaser.Scene {
@@ -30,13 +31,21 @@ export class MainScene extends Phaser.Scene {
         var bottom = bounds.height;
         this.add.existing(new Spawner(this,middle,bottom,2));
         this.add.existing(new Spawner(this,middle+100,bottom,3));
+        this.add.existing(new Spawner(this,middle-100,bottom,5));
+
+        
 
         var combiners = this.physics.add.staticGroup();
         this.bubbles = this.physics.add.group();
 
         combiners.add(new Combiner(this,middle+50,bottom-500,2,3),true);
         this.physics.add.collider(this.bubbles, combiners,Combiner.handleCollision);
-        console.log('physics',this.physics)
+
+        var multipliers = this.physics.add.staticGroup();
+        multipliers.add(new Multiplier(this,middle+50,bottom-200),true);
+        this.physics.add.collider(this.bubbles, multipliers,Multiplier.handleCollision);
+        
+        
         //https://github.com/phaserjs/examples/blob/00868cfbe0ce555c8e9f8ace3e3dc7d1504425ca/public/src/games/firstgame/part9.html#L57
 
         this.add.existing(new HUD(this));
--- js/mech/Multiplier.js
+++ js/mech/Multiplier.js
@@ -10,7 +10,7 @@ const colourChoices = [
     0xa9deec
 ]
 //Combines A*B
-export class Combiner extends Phaser.GameObjects.Rectangle {
+export class Multiplier extends Phaser.GameObjects.Rectangle {
     a;
     b;
     aStorage = 0;
@@ -19,38 +19,68 @@ export class Combiner extends Phaser.GameObjects.Rectangle {
     //group;
     Scene;
     text;
-    spawnSpeed = 1000;
+    spawnSpeed = 2000;
     countdown = 0;
     bubbleCount = 0;
-    constructor(scene, x, y, a, b) {
-        console.log('Combiner constructor');
+    constructor(scene, x, y) {
+        console.log('Multiplier constructor');
 
-        super(scene, x, y, 300, 100, 0x000000);
+        super(scene, x, y, 200, 100, 0x000000);
 
-        this.a = a;
-        this.b = b;
+        this.a = 0;
+        this.b = 0;
         this.Scene = scene;
+
+        this.setInteractive();
+        scene.input.setDraggable(this);
+
+        // Add drag event listeners to the new icon
+        this.on('drag', (pointer, dragX, dragY) => {
+            this.x = dragX;
+            this.y = dragY;
+
+            // if it is over the hud, turn off the animation and change the texture to trash
+            if (this.x < hudWidth + scene.cameras.main.scrollX) {
+                //this.setTexture('trash');
+                //this.anims.stop();
+                //hoveredHud = true;
+            } else {
+                //newRightFanIcon.setTexture('fanAnim');
+                //newRightFanIcon.anims.play('fanSpin');
+                //hoveredHud = false;
+            }
+        });
 
     }
 
     static handleCollision(object1, object2) {
-        //console.log('handleCollision', object1, object2);
+        console.log('handleCollision', object1.value, object2.a,object2.b);
         if (object1.value === object2.a) {
             object2.aStorage++;
         }
         else if (object1.value === object2.b) {
             object2.bStorage++;
         }
+        else if (object2.aStorage <= object2.bStorage)
+        {
+            object2.a = object1.value;
+            object2.aStorage = 1;
+        }
+        else
+        {
+            object2.b = object1.value;
+            object2.bStorage = 1;
+        }
+
         console.log('Collision', object2.aStorage, object2.bStorage);
         object1.destroy();
-
     }
 
     preUpdate(time, delta) {
         if (!this.initialized) {
             this.initialized = true;
             const randomColor = colourChoices[Math.floor(Math.random() * colourChoices.length)];
-            this.Scene.add.existing(new Phaser.GameObjects.Rectangle(this.scene, this.x, this.y, 290, 90, randomColor));
+            this.Scene.add.existing(new Phaser.GameObjects.Rectangle(this.scene, this.x, this.y, 190, 90, randomColor));
 
 
             this.text = this.Scene.add.text(this.x, this.y, "Inactive", {
@@ -71,30 +101,30 @@ export class Combiner extends Phaser.GameObjects.Rectangle {
         }
 
         if (this.aStorage > 0 && this.bStorage > 0) {
-            //this.text.setText(`Active ${this.a}:${this.aStorage} ${this.b}:${this.bStorage}`);
-            this.text.setText(`Active`);
+            this.text.setText(`${this.a}:${this.aStorage} * ${this.b}:${this.bStorage}`);
+            
             this.setFillStyle(0x00FF00);
 
             this.countdown -= delta;
             if (this.countdown <= 0) {
                 this.aStorage--;
                 this.bStorage--;
-                console.log('this.bubbleCount', this.bubbleCount);
+                //console.log('this.bubbleCount',this.bubbleCount);
                 this.countdown = this.spawnSpeed;
-                var bubble = new Bubble(this.Scene, this.x, this.y - (this.height / 2) - 25, this.a * this.b);
+                var bubble = new Bubble(this.Scene, this.x, this.y - (this.height/2) - 25, this.a * this.b);
                 bubble.id = this.bubbleCount++;
                 this.Scene.bubbles.add(bubble, true);
             }
         }
         else {
             var needed = this.a * this.b;
-            this.text.setText(`Provide Numbers that multiply to ${needed}`);
+            this.text.setText(`Multiply two numbers`);
             this.setFillStyle(0x000000);
         }
 
         Phaser.Display.Align.In.Center(this.text, this);
 
-
+        
 
 
     }
